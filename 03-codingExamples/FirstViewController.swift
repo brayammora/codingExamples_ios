aa0105f184970ea3be41ce4c958c0654a0d7ac35
Brayam Alberto Mora Arias <bryanmora22@gmail.com>
2020-02-25 18:25:20 -0500

    Initial Commit

---
+++ 03-codingExamples/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  03-codingExamples
+//
+//  Created by Brayam Alberto Mora Arias on 25/02/20.
+//  Copyright © 2020 Brayam Alberto Mora Arias. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 03-codingExamplesTests/_3_codingExamplesTests.swift
@@ -0,0 +1,34 @@
+//
+//  _3_codingExamplesTests.swift
+//  03-codingExamplesTests
+//
+//  Created by Brayam Alberto Mora Arias on 25/02/20.
+//  Copyright © 2020 Brayam Alberto Mora Arias. All rights reserved.
+//
+
+import XCTest
+@testable import _3_codingExamples
+
+class _3_codingExamplesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
